35979de6c487729b1b474c116b06ea6fdf44e37a
douglasaly <douglasaly2@icloud.com>
2026-06-26 08:34:14 +0200

    f

--- src/app/produtos/[productId]/page.tsx
+++ src/app/produtos/[productId]/page.tsx
@@ -1,13 +1,11 @@
-import { ProductView } from '@/modules/product/ui/views/product-view'
+import { ProductDetailView } from '@/modules/product/ui/views/product-detail-view'
 
 interface PageProps {
 	params: Promise<{ productId: string }>
 }
 
-const Page = async ({ params }: PageProps) => {
+export default async function Page({ params }: PageProps) {
 	const { productId } = await params
 
-	return <ProductView productId={productId} />
+	return <ProductDetailView id={productId} />
 }
-
-export default Page
