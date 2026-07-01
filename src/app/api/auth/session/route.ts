56dd4c3ec60eb6e9aa52821b3bcb9c211e58b66a
Urassamx7 <marvin.lagas01234@gmail.com>
2026-07-01 19:10:06 +0200

    Comment

--- src/app/api/auth/session/route.ts
+++ src/app/api/auth/session/route.ts
@@ -3,7 +3,7 @@ import { createSession } from '@/utils/cookies'
 
 export async function POST(request: Request) {
 	const { token } = await request.json()
-
+	
 	try {
 		await createSession(token)
 		return NextResponse.json({ status: 'login success.' })
